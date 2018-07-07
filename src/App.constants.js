831da19f9b2c78b143c338ac3eadac12d75f7848
Jose Pol Sastre <polsastre3@gmail.com>
2018-07-07 10:24:53 +0200

    endpoint in prod updated

--- src/App.constants.js
+++ src/App.constants.js
@@ -1,1 +1,1 @@
-export const ENDPOINT = process.env.NODE_ENV === 'development' ? 'http://localhost:3900' : 'buggify.api.polsastre.com'
+export const ENDPOINT = process.env.NODE_ENV === 'development' ? 'http://localhost:3900' : 'http://buggify.api.polsastre.com'
